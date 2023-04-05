dec5c9c866df0f7b03c844aad7f10f496738cc78
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2023-04-05 16:56:50 -0400

    Add project files.

---
+++ Lecture1_BankAccount/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1_BankAccount
+{
+    public class BankAccount
+    {
+        // Fields - fields have lowercased first letter
+        float balance;
+
+
+        // Properties - properties have uppercased first letter
+        public float Balance   
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+
+        // Methods
+        public void Debit(float debitAmount)  // takes in a debit amount and subtracts from 'balance' 
+        {
+            balance -= debitAmount;
+        }
+    }
+}
